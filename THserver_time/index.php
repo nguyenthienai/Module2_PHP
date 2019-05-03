f7c1012cc007348fd766b3d3d3fad30346a6a4e9
nguyenthienai <nguyenthienai.nta@gmail.com>
2019-05-03 20:48:07 +0700

    nop bai

---
+++ THserver_time/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Thời gian hiện tại</title>
+</head>
+<body>
+    <?php echo 'Xin chao' ; ?>
+    <h1>Bây gờ là: <?php echo date('Y-M-d H:m:s'); ?></h1>
+</body>
+</html>
