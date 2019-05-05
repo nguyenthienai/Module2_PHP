2945b0fd438f0729c5aa99ac91734e72fa8390ad
nguyenthienai <nguyenthienai.nta@gmail.com>
2019-05-05 21:14:48 +0700

    luyện tập dùng lệnh include tạo layout

---
+++ TH_TaoLayout/index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css"href="css/style.css">
+    </head>
+    <title>Laout</title>
+    <body>
+        <div class="container">
+            <header><?php  include "header.php";  ?></header>
+            <nav><?php include "nav.php";  ?></nav>
+            <article> <?php include "content.php"; ?></article>
+            <footer><?php include "footer.php";?></footer>
+        </div>
+    </body>
+</html>
